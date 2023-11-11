e16e6bdb0daeabe5a835a90e9e01bea04acb5c7c
Charger1256A <aditya.ram112005@gmail.com>
2023-11-11 13:20:15 -0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Home from "./screens/home/home.jsx";
+import Login from "./screens/login/login.jsx";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      {user ? (
+        <Route path="/" element={<Login />}></Route>
+      ) : (
+        <Route path="/" element={<Login />}></Route>
+      )}
+    </Routes>
+  )
 }
-
-export default App;
---
+++ src/screens/login/login.jsx
@@ -0,0 +1,16 @@
+import "./login.css"
+
+export default function Login() {
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      {/* <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="With sx" variant="standard" />
+      </Box> */}
+      
+
+
+    </div>
+  );
+}
