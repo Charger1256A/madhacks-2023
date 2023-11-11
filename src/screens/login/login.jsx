d800deba299e88e9a6b9d75a599f3c36dacba4a8
Charger1256A <aditya.ram112005@gmail.com>
2023-11-11 14:11:07 -0600

    completed login screen

--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
 import "./login.css"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 
 export default function Login() {
+  const [mode, setMode] = useState("login");
+  const[username, setUsername] = useState("");
+  const[password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  
+
   return (
     <div className="container">
-      <h1>Login</h1>
-      {/* <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label="With sx" variant="standard" />
-      </Box> */}
       
+     
+        {mode === "login" ? (
+          <div className="box">
+            <Button className="sign-up" onClick={() => setMode("signup")}>Sign Up</Button>
+            <h1>Login</h1>
+              <TextField className="login-content" id="input-with-sx" label="Enter username" variant="standard" value={username} onChange={(e) => setUsername(e.target.value)}/>
+  
+            <TextField className="login-content" id="standard-basic" label="Enter password" variant="standard" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+  
+            <Button className="login-content">Login</Button>
+           </div>
+        ) : (
+          <div className="box">
+            <Button className="sign-up" onClick={() => setMode("login")}>Back</Button>
+            <h1>Signup</h1>
+                <TextField className="login-content" id="input-with-sx" label="Enter username" variant="standard" value={username} onChange={(e) => setUsername(e.target.value)}/>
+      
+    
+              <TextField className="login-content" id="standard-basic" label="Enter password" variant="standard" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <TextField className="login-content" id="standard-basic" label="Confirm password" variant="standard" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+              
+    
+              <Button className="login-content">Signup</Button>
+           </div>
+        )}
+       
 
 
     </div>
