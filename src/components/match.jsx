fffd8787e7f5bb5ba95404c2802307e804fde785
Aaryan Patel <aaryanpatel@Aaryans-MacBook-Pro.local>
2023-11-11 15:51:40 -0600

    offline match card completion

---
+++ src/components/match.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
+
+export default function Match() {
+  return (
+    <Card sx={{ minWidth: 250 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 20 }} color="black" gutterBottom>
+          Match ID
+        </Typography>
+        <TextField className="matcher" id="input-with-sx" label="Enter Match ID" variant="standard"/>
+      </CardContent>
+      <CardActions>
+        <Button size="large">Submit</Button>
+      </CardActions>
+    </Card>
+  );
+}
